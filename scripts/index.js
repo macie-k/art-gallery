c80161038d6489c205408ab1b7f9ca60236733d6
emermacko <mr.macko.yt@gmail.com>
2020-08-20 16:06:21 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ var subnav_animated = false;
 var initial_scrolled;
 
 $(document).ready(function(){
-    imgMargin();
     if($(window).scrollTop() < "50") {
         initial_scrolled = false;
     } else {
@@ -11,12 +10,10 @@ $(document).ready(function(){
     }
     if(isDesktop()) {
         $('#intro-content').height($('#intro img').height() + 200);
-        $('#intro').height($(window).height() - $('#navbar').height());
     }
 });
 
 $(window).resize(function(){
-    imgMargin();
     if(!subnav_active) {
         if(isDesktop()) {
             $('#subnav').fadeIn(600);
@@ -24,22 +21,35 @@ $(window).resize(function(){
         } else $('.nav-item').css('right', '-100px');
     }
     if(isDesktop()) {
-        $('#intro-content').height($('#intro img').height() + 200);
-        $('#intro').height($(window).height() - $('#navbar').height());
+        $('#intro-content').height($('#intro-img').height() + 200);
+    } else {
+        $('#bounce-arrow').hide();
     }
 })
 
 $(window).scroll(function(){
-    if($(window).scrollTop() < "50") {
-        initial_scrolled = false;
-    }
-    if(isVisible('#separators', -100) && !initial_scrolled) {
-        $('html').animate({
-            scrollTop: $('#about-me-txt').offset().top - 50
-        }, 500, 'swing');
-        initial_scrolled = true;
+    if(!isDesktop()) {
+        if($(window).scrollTop() < "50") {
+            initial_scrolled = false;
+        }
+        if(isVisible('#separators', -100) && !initial_scrolled) {
+            $('html').animate({
+                scrollTop: $('#about-me-txt').offset().top - 50
+            }, 500, 'swing');
+            initial_scrolled = true;
+        }
+    } else {
+        if($(window).scrollTop() < "100") {
+            $('#bounce-arrow').fadeIn(500);
+        } else {
+            if($('#bounce-arrow').is(":visible")) {
+                $('#bounce-arrow').fadeOut(500);
+            }
+        }
+
     }
 });
+
 
 const subnav = $('#subnav');
 const subnav_items = $('.nav-item');
@@ -91,10 +101,6 @@ function isVisible(elem, threshold) {
 /* checking if website is displayed on mobile or desktop */
 function isDesktop() {
   return ($(window).width() >= 700);
-}
-
-function imgMargin() {
-    $('#first img').css('margin-right', ($(window).width() - $('#first img').width())/2);
 }
 
 /* disable dragging */
